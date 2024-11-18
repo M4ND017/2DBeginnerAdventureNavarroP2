170d0226ba9c372e240493abd1edfcd8469e6dd5
M4ND017 <732296@student.dusd.net>
2024-11-18 10:21:56 -0800

    Finished Ducko animation
    I finished the Ducko and Enemy animation

--- 2DBeginnerAdventureNavarroP2/Assets/Scripts/DuckoController.cs
+++ 2DBeginnerAdventureNavarroP2/Assets/Scripts/DuckoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
@@ -19,12 +20,15 @@ public class DuckoController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-
+        animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
@@ -32,6 +36,17 @@ public class DuckoController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+       
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude); 
 
         if (isInvincable)
         {
@@ -55,14 +70,16 @@ public class DuckoController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincable)
             {
                 return;
             }
             isInvincable = true;
             InvincableTimer = timeInvincable;
-            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-            Debug.Log(currentHealth + "/" + maxHealth);
+           
         }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
